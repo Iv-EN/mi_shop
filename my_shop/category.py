43b4e0b64f90c82b2bc1f4c5838229dc37be6cde
Iv-EN <engenivanov@yandex.ru>
2024-03-29 12:12:32 +0300

    Completed task 1

--- my_shop/category.py
+++ my_shop/category.py
@@ -3,18 +3,26 @@ from .product import Product
 
 class Category:
     """Класс для представления категорий."""
-    name: str
-    description: str
-    goods: list[Product]
     count_categories = 0
     count_unique_products = 0
     unique_products = set()
 
-    def __init__(self, name, description, goods):
-        self.name = name
-        self.description = description
-        self.goods = goods
-
+    def __init__(self, name, description, goods=None):
+        if goods is None:
+            goods = []
+        self.name: str = name
+        self.description: str = description
+        self.__goods: list[Product] = goods
         Category.count_categories += 1
-        Category.unique_products.update(set(self.goods))
+        Category.unique_products.update(set(self.__goods))
         Category.count_unique_products = len(Category.unique_products)
+
+    @property
+    def add_product(self, product: Product):
+        self.__goods.append(product)
+        Category.unique_products.add(product)
+        Category.count_unique_products = len(Category.unique_products)
+
+    @property
+    def goods(self):
+        return self.__goods
