3d0d20bdf6e4bed08e34ab2d55207d6e3351083c
Iv-EN <engenivanov@yandex.ru>
2024-04-04 23:44:11 +0300

    added a test for an empty category

--- tests/test_product_iterator.py
+++ tests/test_product_iterator.py
@@ -1,3 +1,5 @@
+import pytest
+
 from my_shop.category import Category
 from my_shop.product import Product
 from my_shop.product_iterator import ProductIterator
@@ -8,6 +10,11 @@ def test_product_iterator():
     product2 = Product("Ботинки", "Повседневные", 7500, 2)
     goods = [product1, product2]
     category1 = Category("Обувь", "Все для ног", goods)
+    category2 = Category("Обувь", "Все для ног", [])
     iterator = ProductIterator(category1)
     for good in goods:
         assert next(iterator) == good
+    category2 = Category("Обувь", "Все для ног", [])
+    iterator2 = ProductIterator(category2)
+    with pytest.raises(StopIteration):
+        next(iterator2)
